12075a154ef87d62fa519076609d5deef603ff38
rvelfre <rvelfre@maprtech.com>
2018-06-27 15:58:47 +0200

    Create JAVA consumers

---
+++ src/main/java/com/mapr/examples/DBConsumer_audit_fs.java
@@ -0,0 +1,62 @@
+package com.mapr.examples;
+
+import com.mapr.db.MapRDB;
+import com.mapr.db.Table;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+
+public class DBConsumer_audit_fs {
+
+  public static void main(String[] args) {
+
+    Table table = getTable("/apps/demo.mapr.com_fs_maprdemo");
+
+
+    KafkaConsumer<String, String> consumer;
+    Properties properties = new Properties();
+    properties.setProperty("group","demo.mapr.com_fs_maprdemo");
+    properties.setProperty("enable.auto.commit","true");
+    properties.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+    properties.setProperty("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+    properties.setProperty("auto.offset.reset", "latest");
+
+    consumer = new KafkaConsumer<>(properties);
+
+    consumer.subscribe(Collections.singletonList("/var/mapr/auditstream/auditlogstream:demo.mapr.com_fs_maprdemo"));
+
+    int i=0;
+
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(200);
+      for (ConsumerRecord<String, String> record : records) {
+        System.out.println(record.topic() +":"+  record.value());
+        table.insertOrReplace(UUID.randomUUID().toString(), MapRDB.newDocument( record.value() ) );
+        i = i++;
+        if (i==10) {
+          table.flush();
+          i=0;
+        }
+      }
+    }
+
+  }
+
+  public static Table getTable(String tableName) {
+
+    if (MapRDB.tableExists(tableName)) {
+      return MapRDB.getTable(tableName);
+    } else {
+      return MapRDB.createTable(tableName);
+    }
+
+
+  }
+
+
+}
